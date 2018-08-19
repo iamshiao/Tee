9b3747ec84b4eded8b1a22c5c5f363814eb0b0ef
Circle Hsiao <iamshiao@gmail.com>
2018-08-19 16:18:23 +0800

    Add DateTime to each row.

--- Tee/Program.cs
+++ Tee/Program.cs
@@ -15,13 +15,23 @@ namespace Tee
             {
                 string logFilePath = Path.GetFullPath(args[0]);
 
+                string sentence = "";
                 using (StreamWriter writer = new StreamWriter(logFilePath, true))
                 {
                     for (int value; (value = Console.In.Read()) != -1;)
                     {
                         var word = Char.ConvertFromUtf32(value);
-                        Console.Write(word);
-                        writer.Write(word);
+
+                        if (word == "\n" && Console.In.Peek() == -1)
+                        {
+                            writer.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss : ") + sentence);
+                            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss : ") + sentence);
+                            sentence = "";
+                        }
+                        else
+                        {
+                            sentence += word;
+                        }
                     }
                 }
             }
